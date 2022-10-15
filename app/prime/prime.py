ce6a4bac8581baf38a1e4c6dd6711e1e326b00d4
Rafał Stasiowski <39459132+RafalStasiowski@users.noreply.github.com>
2022-10-15 15:08:43 +0200

    prime number checker improvement

--- app/prime/prime.py
+++ app/prime/prime.py
@@ -1,7 +1,16 @@
-def is_prime(n):
-    for i in range(2, n):
-        if (n % i) == 0:
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif (n%2 == 0) or (n%3 == 0):
+        return False
+    i = 5
+    while i * i <= n:
+        if (n%i == 0) or (n%(i + 2) == 0):
             return False
+        i = i + 6
+
     return True
 
 
