5ef1ab94bc4b961d09b11688115193c22a773318
Rafał Stasiowski <rafal.tomasz.stasiowski@gmail.com>
2022-12-03 19:41:27 +0100

    deploy hotfix

--- app/inverting_picture/inverting_picture.py
+++ app/inverting_picture/inverting_picture.py
@@ -2,7 +2,8 @@ import cv2
 
 
 def invert():
-    image = cv2.imread("img.jpg")
-    image = ~image
-    cv2.imwrite("img_inv.jpg", image)
-    return image
+    return {}
+    # image = cv2.imread("img.jpg")
+    # image = ~image
+    # cv2.imwrite("img_inv.jpg", image)
+    # return image
